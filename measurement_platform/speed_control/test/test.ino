3cedcc27c7f719f8bb2aad0e805bb23ad98777ec
Max Johansson <contact@maxjohansson.se>
2020-03-09 14:31:44 +0100

    Encoder can output speed

--- measurement_platform/speed_control/test/test.ino
+++ measurement_platform/speed_control/test/test.ino
@@ -13,18 +13,20 @@ volatile byte reading = 0; //somewhere to store the direct values we read from o
 
 int oldTime = 0;
 int newTime = 0;
-int speedd = 0;
+int elapsed = 0;
 
 void setup() {
   pinMode(pinA, INPUT_PULLUP); // set pinA as an input, pulled HIGH to the logic voltage (5V or 3.3V for most cases)
   pinMode(pinB, INPUT_PULLUP); // set pinB as an input, pulled HIGH to the logic voltage (5V or 3.3V for most cases)
-  attachInterrupt(0,PinA,RISING); // set an interrupt on PinA, looking for a rising edge signal and executing the "PinA" Interrupt Service Routine (below)
-  attachInterrupt(1,PinB,RISING); // set an interrupt on PinB, looking for a rising edge signal and executing the "PinB" Interrupt Service Routine (below)
+  attachInterrupt(digitalPinToInterrupt(pinA),PinA,RISING); // set an interrupt on PinA, looking for a rising edge signal and executing the "PinA" Interrupt Service Routine (below)
+  attachInterrupt(digitalPinToInterrupt(pinB),PinB,RISING); // set an interrupt on PinB, looking for a rising edge signal and executing the "PinB" Interrupt Service Routine (below)
   Serial.begin(9600); // start the serial monitor link
   Serial.println("Starting");
 }
 
-void PinA(){  //frammåt
+
+
+void PinA2(){  //bakåt
   cli(); //stop interrupts happening before we read pin values
   reading = PIND & 0xC; // read all eight pin values then strip away all but pinA and pinB's values
   if(reading == B00001100 && aFlag) { //check that we have both pins at detent (HIGH) and that we are expecting detent on this pin's rising edge
@@ -32,34 +34,38 @@ void PinA(){  //frammåt
     bFlag = 0; //reset flags for the next turn
     aFlag = 0; //reset flags for the next turn
     newTime = (millis());
-    speedd = newTime - oldTime;
+    elapsed = newTime - oldTime;
+    Serial.print("pin A:  ");
+    Serial.println(elapsed);
   }
   else if (reading == B00000100) bFlag = 1; //signal that we're expecting pinB to signal the transition to detent from free rotation
-  oldTime = (0.001*millis());
+  oldTime = (millis());
 
   sei(); //restart interrupts
 }
 
-void PinB(){  //bakåt
+void PinB(){  //frammåt
   cli(); //stop interrupts happening before we read pin values
   reading = PIND & 0xC; //read all eight pin values then strip away all but pinA and pinB's values
   if (reading == B00001100 && bFlag) { //check that we have both pins at detent (HIGH) and that we are expecting detent on this pin's rising edge
     encoderPos ++; //increment the encoder's position count
     bFlag = 0; //reset flags for the next turn
     aFlag = 0; //reset flags for the next turn
-    newTime = (0.001*millis());
-    speedd = newTime - oldTime;
+    newTime = (millis());
+    elapsed = newTime - oldTime;
+    Serial.print("pin B:  ");
+    Serial.println(elapsed);
   }
   else if (reading == B00001000) aFlag = 1; //signal that we're expecting pinA to signal the transition to detent from free rotation
-  oldTime = (0.001*millis());
+  oldTime = (millis());
   
   sei(); //restart interrupts
 }
 
 void loop(){
   if(oldEncPos != encoderPos) {
-    Serial.println(encoderPos);
-    Serial.println(speedd);
+    //Serial.println(encoderPos);
+    //Serial.println(elapsed);
     oldEncPos = encoderPos;
   }
 }
